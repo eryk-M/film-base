44c033c4630212e839a61b69aa5ad8e008a5b5c1
Eryk <chiracle@gmail.com>
2019-03-24 22:18:51 +0100

    discover #1

--- src/actions/discover.actions.js
+++ src/actions/discover.actions.js
@@ -1,9 +1,16 @@
 export const FETCH_DISCOVER = "FETCH_DISCOVER";
 
-export const getDiscover = (api, type, page) => {
+export const getDiscover = (
+  api,
+  page,
+  sortBy,
+  voteAverage,
+  yearMin,
+  yearMax
+) => {
   return dispatch => {
     fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=${type}&include_adult=false&include_video=false&page=${page}`
+      `https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=${sortBy}&include_adult=false&include_video=false&page=${page}&vote_average.gte=${voteAverage}&primary_release_date.gte=${yearMin}-01-01&primary_release_date.lte=${yearMax}`
     )
       .then(res => res.json())
       .then(discover => {
--- src/pages/Discover/Discover.js
+++ src/pages/Discover/Discover.js
@@ -5,21 +5,174 @@ import { getApi } from "../../actions/api.actions";
 import { getGenres } from "../../actions/genres.actions";
 import { getDiscover } from "../../actions/discover.actions";
 import "../MainStyling.scss";
-
+import "./Discover.scss";
+import DiscoverItem from ".././Discover/DiscoverItem/DiscoverItem";
+import Nouislider from "nouislider-react";
+import "nouislider/distribute/nouislider.css";
 class Discover extends Component {
   state = {
-    type: "popularity.desc",
-    page: 1
+    sortBy: "popularity.desc",
+    voteAverage: 0,
+    page: 1,
+    results: [],
+    totalPages: 0,
+    yearMin: 2000,
+    yearMax: 2019
   };
 
   componentDidMount() {
     this.props.getApi();
-    this.props.getDiscover(this.props.api, this.state.type, this.state.page);
+    this.props.getDiscover(
+      this.props.api,
+      this.state.page,
+      this.state.sortBy,
+      this.state.voteAverage,
+      this.state.yearMin,
+      this.state.yearMax
+    );
+    this.props.getGenres(this.props.api);
   }
+  componentWillReceiveProps(newProps) {
+    if (newProps.discover.total_pages !== this.state.totalPages) {
+      this.setState({
+        totalPages: newProps.discover.total_pages,
+        page: newProps.discover.page
+      });
+    }
+  }
+  handleGetDiscover = page => {
+    this.props.getDiscover(
+      this.props.api,
+      page,
+      this.state.sortBy,
+      this.state.voteAverage,
+      this.state.yearMin,
+      this.state.yearMax
+    );
+  };
+
+  handlePagination = type => {
+    const { page } = this.state;
+    switch (type) {
+      case "add":
+        this.setState({ page: page + 1 });
+        this.handleGetDiscover(page);
+        break;
+      case "minus":
+        if (page !== 1) {
+          this.setState({ page: page - 1 });
+          this.handleGetDiscover(page);
+        }
+        break;
+      default:
+        return null;
+    }
+  };
+  handleChange = e => {
+    this.setState({ voteAverage: e.target.value });
+  };
+  handleYearChange = e => {
+    this.setState({
+      year: e.target.value
+    });
+  };
+  handleSelectChange(e) {
+    this.setState({
+      sortBy: e.target.value
+    });
+  }
+  onSlide = (render, handle, value, un, percent) => {
+    const min = parseInt(value[0].toFixed());
+    const max = parseInt(value[1].toFixed());
+    this.setState({
+      yearMin: min,
+      yearMax: max
+    });
+  };
   render() {
+    const discover = this.props.discover.results;
+    const btnOff = {
+      opacity: 0.5
+    };
     return (
       <div className="main">
-        <h1>im on discover page</h1>
+        <h1 className="main__heading">DISCOVER MOVIES</h1>
+        <div className="main__sort">
+          <div className="main__sort-range">
+            <span>Vote Average</span>
+            <span id="rangeValue">{this.state.voteAverage}</span>
+            <input
+              className="range"
+              type="range"
+              value={this.state.voteAverage}
+              min="0"
+              max="10"
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <div className="main__sort-year">
+            <span className="main__sort-info--1">
+              From: {this.state.yearMin}
+            </span>
+            <span className="main__sort-info--2">To: {this.state.yearMax}</span>
+            <Nouislider
+              connect
+              start={[2000, 2019]}
+              behaviour="tap"
+              range={{
+                min: [1944],
+                max: [2019]
+              }}
+              onSlide={this.onSlide}
+            />
+          </div>
+          <div className="main__sort-select">
+            <select onClick={this.handleSelectChange.bind(this)}>
+              <option value="popularity.desc">Popularity DESC</option>
+              <option value="popularity.asc">Popularity ASC</option>
+              <option value="release_date.desc">Release date DESC</option>
+              <option value="release_date.asc">Release date ASC</option>
+              <option value="original_title.asc">Original title ASC</option>
+              <option value="vote_average.asc">Vote average ASC</option>
+            </select>
+          </div>
+        </div>
+        <div className="main__sort-search">
+          <button className="main__sort-search-btn">Search</button>
+        </div>
+        <div className="main__sort-buttons">
+          <button
+            style={this.state.page === 1 ? btnOff : null}
+            onClick={() => {
+              this.handlePagination("minus");
+              this.handleGetDiscover();
+            }}
+          >
+            Previous
+          </button>
+          <button
+            onClick={() => {
+              this.handlePagination("add");
+              this.handleGetDiscover();
+              // this.plus();
+            }}
+          >
+            Next
+          </button>
+        </div>
+        <p
+          style={{
+            fontSize: 30
+          }}
+        >
+          Strona: {this.state.page}
+          wszystkich stron: {this.props.discover.total_pages}
+        </p>
+
+        <div className="main__container">
+          <DiscoverItem genres={this.props.genres.genres} discover={discover} />
+        </div>
       </div>
     );
   }
